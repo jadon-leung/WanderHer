9d8c682580cef9d76712bd0ad743ab0ab96d6b96
jadon-leung <146619444+jadon-leung@users.noreply.github.com>
2024-02-17 20:11:17 -0800

    big changes to database and profile creation

--- server.py
+++ server.py
@@ -3,6 +3,25 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/match')
+def match():
+    return render_template('match.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 def hello_world():
     return "<p>Hello, World!</p>"
 if __name__ == "__main__":
